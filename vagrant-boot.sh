abf096f41d2529a626443811fe7b1afbe4a31970
Hans Kristian Flaatten <hans@starefossen.com>
2013-08-29 16:17:42 +0200

    Update Vagrant setup

--- vagrant-boot.sh
+++ vagrant-boot.sh
@@ -1,8 +1,5 @@
 apt-get update
-apt-get install -y ruby1.9.1
-
+apt-get install -y make ruby1.9.1-dev
 echo "installing ruby gems"
-gem install nanoc
-gem install adsf
+gem install jekyll
 gem install github-pages
-gem install kramdown
