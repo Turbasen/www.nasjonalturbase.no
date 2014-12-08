e9bd61ec7a2f308c5f9ce3ec41d2acc7a8e06c1c
Hans Kristian Flaatten <hans.kristian.flaatten@turistforeningen.no>
2014-12-08 21:41:34 +0100

    Add inline preview of thumbnails

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 // Highlight source code
 hljs.initHighlightingOnLoad();
 
+// Thumbnail inline preview
+$('.thumbnail-preview').on('click', function(e) {
+  e.preventDefault();
+
+  var width = $(this).css('width');
+  var orgWidth = $(this).data('orgWidth') || '';
+
+  if (orgWidth) {
+    $(this).data('orgWidth', '');
+    $(this).animate({ 'width': orgWidth }, function() {
+      $(this).css('width', '');
+    });
+  } else {
+    $(this).data('orgWidth', width);
+    $(this).animate({ 'width': '100%' });
+  }
+});
+
 // Enable all tooltips by default
 $('a[data-toggle="tooltip"]').tooltip();
 
